ff786254d543d970f62d7a5b8d4a974f4f61caa2
OwenShuaiLiu <owen.shuai.liu@berkeley.edu>
2021-06-28 23:04:58 -0700

    initial commit

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,21 +7,21 @@ const PROJECTS = [
        id: 1,
        title: 'Example React Application',
        description: 'A React App that I built, involving JS and core web dev concepts.',
-       link: 'https://github.com/15DKatz/example/',
+       link: 'https://github.com/OwenShuaiLiu/portfolio-v',
        image: project1
    },
    {
        id: 2,
        title: 'My API',
        description: 'A REST API that I built from scratch with GET and POST requests.',
-       link: 'https://github.com/15DKatz/example/',
+       link: 'https://github.com/OwenShuaiLiu/Boomerang_Back_End',
        image: project2
    },
    {
        id: 3,
-       title: 'Operating Systems Final Project',
+       title: 'Analytics Project',
        description: 'My unique final project for my university Operating Systems course',
-       link: 'https://github.com/15DKatz/example/',
+       link: 'https://github.com/OwenShuaiLiu/capstone_demand_modeling',
        image: project3
    }
 ];
